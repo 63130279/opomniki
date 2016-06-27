08cb0e233c017d562131f0e927c858c023eb2cf6
63130279 <jz2092@student.uni-lj.si>
2016-06-27 18:22:04 +0000

    Stran pripravljena za produkcijo

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -10,13 +10,20 @@ window.addEventListener('load', function() {
 	}
 	
 	document.querySelector("#dodajGumb").addEventListener('click', dodajOpomnik); 
+	var naziv;
+	var cas;
+	function getValues(){
+		naziv = document.querySelector("#naziv_opomnika").value;
+		cas = document.querySelector("#cas_opomnika").value;		
+	}
+
 	function dodajOpomnik(){
-		var naziv = document.querySelector("#naziv_opomnika").value;
-		var cas = document.querySelector("#cas_opomnika").value;
+		getValues();
 		document.querySelector("#naziv_opomnika").value = "";
 		document.querySelector("#cas_opomnika").value = "";
-		
-		document.querySelector("#opomniki").innerHTML = "<div class='opomnik'> <div class='naziv_opomnika'>" +naziv+ "</div> <div class='cas_opomnika'> Opomnik čez <span> "+cas+"</span> sekund. </div> </div>";
+		var vsebina = document.querySelector("#opomniki").innerHTML;
+		document.querySelector("#opomniki").innerHTML = vsebina+"<div class='opomnik'> <div class='naziv_opomnika'>" +naziv+ "</div> <div class='cas_opomnika'> Opomnik čez <span> "+cas+"</span> sekund. </div> </div>";
+
 	}
 		
 	//Posodobi opomnike
@@ -28,6 +35,17 @@ window.addEventListener('load', function() {
 			var casovnik = opomnik.querySelector("span");
 			var cas = parseInt(casovnik.innerHTML);
 	
+			if(cas == 0)
+			{
+				var message = "Opomnik!\n\nZadolžitev "+naziv+" je potekla!"
+				alert (message);
+				document.querySelector("#opomniki").removeChild(opomnik);
+			}
+			else
+			{
+				casovnik.innerHTML=cas-1;
+				
+			}
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
